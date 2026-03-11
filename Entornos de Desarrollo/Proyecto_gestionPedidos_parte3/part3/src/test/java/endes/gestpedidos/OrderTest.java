de456fbb077b9efbe4580d27bb612a65016f95c1
Juan Francisco González Gómez <gonzalezjf@pm.me>
2026-03-11 14:54:33 +0100

    Part 3 maven project initialized

---
+++ Entornos de Desarrollo/Proyecto_gestionPedidos_parte3/part3/src/test/java/endes/gestpedidos/OrderTest.java
@@ -0,0 +1,20 @@
+package endes.gestpedidos;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class OrderTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testCalcTotal()
+    {
+        assertTrue( true );
+    }
+}
